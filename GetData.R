001a69804a25de2b955709f9a50a4fd790bd92c1
adilchhabra <adilchhabra7@gmail.com>
2018-12-09 15:19:02 -0500

    Selected YouTubers to analyze Got channel stats

--- GetData.R
+++ GetData.R
@@ -28,3 +28,21 @@ barplot(100*colSums(score)/sum(score),
         las = 2,
         col = rainbow(10),
         ylab = 'Percentage',main = 'Sentiment Scores for Youtube Comments')
+
+# Channels Picked
+# Two incredibly popular right leaning youtubers:
+# Steven Crowder - https://www.youtube.com/channel/UCIveFvW-ARp_B_RckhweNJw
+# Ben Shapiro - https://www.youtube.com/channel/UCnQC_G5Xsjhp9fEJKuIcrSw
+# Two incredibly popular left leaning youtubers:
+# Contrapoints - https://www.youtube.com/user/ContraPoints
+# Shaun - https://www.youtube.com/channel/UCJ6o36XL0CpYb6U5dNBiXHQ
+
+crowderID <- "UCIveFvW-ARp_B_RckhweNJw"
+shapiroID <- "UCnQC_G5Xsjhp9fEJKuIcrSw"
+contraID <- "UCNvsIonJdJ5E4EXMa65VYpA"
+shaunID <- "UCJ6o36XL0CpYb6U5dNBiXHQ"
+
+crowderStats <- get_channel_stats(crowderID)
+shapiroStats <- get_channel_stats(shapiroID)
+contraStats <- get_channel_stats(contraID)
+shaunStats <- get_channel_stats(shaunID)
