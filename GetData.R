5ff8fe439651267cd5636bbefb474d61b681ee67
adilchhabra <adilchhabra7@gmail.com>
2018-12-09 15:09:45 -0500

    Obtaining Authorization

---
+++ GetData.R
@@ -0,0 +1,7 @@
+library(tuber)
+
+#OAuth and Connection
+app_id <- "830360114408-ev1q4bqv76daq7o0i4b2dj48dehomt36.apps.googleusercontent.com"
+app_secret <- "OjLU0iwI5yJjk9axkpwHUjgC"
+yt_oauth(app_id, app_secret)
+
