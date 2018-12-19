e2e1a9a244772436aaf3b8f446f37e1c8b40c1a0
adilchhabra <adilchhabra7@gmail.com>
2018-12-19 00:43:49 -0500

    Final Shiny App - Demo of Sentiment Analysis Model

--- app.R
+++ app.R
@@ -1,5 +1,12 @@
 library(shiny)
 library(tuber)
+library(dplyr)
+library(tibble)
+library(syuzhet)
+library(stringi)
+library(ggplot2)
+library(tidytext)
+library(stats)
 
 #OAuth and Connection
 app_id <- "830360114408-ev1q4bqv76daq7o0i4b2dj48dehomt36.apps.googleusercontent.com"
@@ -10,30 +17,46 @@ yt_oauth(app_id, app_secret)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Get YouTube Channel Data and Comments"),
+   titlePanel("Get YouTube Channel Data and Comments + Sentiment Analysis"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
         textInput(inputId = "channelID", 
-                  label = "Enter Channel ID: ", 
+                  label = "Enter Channel ID: ",
+                  value = "UCwWhs_6x42TyRM4Wstoq8HA",
                   placeholder = "Channel ID"),
         
         dateRangeInput(inputId = "dateRange", 
-                       label = "Enter Date Range") 
+                       label = "Enter Date Range"),
         
+        actionButton(inputId = "showVideos", 
+                     label = "Get Videos"),
+        
+        actionButton(inputId = "showComments", 
+                     label = "Get Comments"),
+        
+        actionButton(inputId = "showPlot", 
+                     label = "Get Sentiment Score")
       ),
       
-      # Show a plot of the generated distribution
       mainPanel(
-         tableOutput('channelStats'),
-         tableOutput('channelVideos')
+        
+        tabsetPanel(type = "tabs",
+                    
+                    tabPanel("Channel and Videos", tableOutput("channelStats"), tableOutput('channelVideos')),
+                    
+                    tabPanel("Comments", tableOutput('videoComments')),
+
+                    tabPanel("Sentiment Analysis", plotOutput('sentimentPlot'))
+                    
       )
    )
-)
+))
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  #get videos within date range
   selectedData <- reactive({
     req(input$channelID)
     req(input$dateRange)
@@ -44,8 +67,21 @@ server <- function(input, output) {
       arrange(date) %>%
       mutate(Title = title,
              Description = description,
-             UploadedOn = publishedAt) %>%
-      select(Title, Description, UploadedOn)
+             UploadedOn = publishedAt,
+             ID = video_id) %>%
+      select(ID, Title, Description, UploadedOn)
+  })
+  
+  #comments data frame
+  allComments <- reactive({
+    comments = lapply(as.character(selectedData()$ID), function(x){
+      get_comment_threads(c(video_id = x), max_results = 1000)
+    })
+    comments_text = lapply(comments,function(x){
+      as.character(x$textOriginal)
+    })
+    comments_text = tibble(text = Reduce(c, comments_text)) %>%
+      mutate(text = stri_trans_general(tolower(text), "Latin-ASCII"))
   })
   
   output$channelStats <- 
@@ -61,8 +97,52 @@ server <- function(input, output) {
     })
   
   output$channelVideos <- 
-    renderTable({ 
-      selectedData()
+    renderTable({
+      if(input$showVideos>0) {
+        selectedData()
+      }
+    })
+  
+  output$videoComments <- 
+    renderTable({
+      if(input$showComments>0) {
+        allComments()
+      }
+    })
+  
+  output$sentimentPlot <- 
+    renderPlot({
+      if(input$showPlot>0) {
+        req(input$channelID)
+        req(input$dateRange)
+        custom_stop_words <- bind_rows(data_frame(word = c("white"), #remove "white" from lexicon
+                                                  lexicon = c("custom")),
+                                       stop_words)
+        
+        tidyComments <- allComments() %>% #break down sentences into words
+          tidytext::unnest_tokens(word, text) %>%
+          anti_join(custom_stop_words, by = "word")
+        
+        commentSentiment <- tidyComments %>% #obtain sentiment scores and order
+          inner_join(get_sentiments("nrc"), by = "word") %>% 
+          count(word, sentiment, sort = TRUE) %>% 
+          group_by(sentiment) %>%
+          top_n(10) %>%
+          ungroup() %>%
+          mutate(pos_neg = ifelse(sentiment %in% c("positive", "anticipation", "joy", "trust", "surprise"), 
+                                  "Positive", "Negative")) 
+        
+        #Plot number of words/sentiment
+        sentiment_Plot <- ggplot(commentSentiment, aes(reorder(sentiment, n), n)) +
+          geom_col(aes(fill = pos_neg), show.legend = FALSE) +
+          scale_fill_manual(values = c("red2", "green3")) +
+          xlab("Sentiment") +
+          ylab("Total Number of Words") + 
+          labs(title = "Total Number of Words by Sentiment or Emotion") +
+          coord_flip()
+        
+        sentiment_Plot
+      }
     })
 }
 
